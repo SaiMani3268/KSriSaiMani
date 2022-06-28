e4621f512ed9ffd0d239b3fda40bb2d9687d0fea
Kovvada Sri Sai Mani <srisaimanikovvada@gmail.com>
2022-06-28 22:56:10 +0530

    fifth commit

---
+++ first-project/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const path = require('path')
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'mustache')
+app.engine('mustache', require('hogan-middleware').__express)
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res, next) => {
+    res.send('Hello!!')
+})
+
+app.get('/json', (req, res, next) => {
+    const data = {
+        greeting: 'Hello!!'
+    }
+    res.json(data)
+})
+
+app.get('/home', (req, res, next) => {
+    res.render('home', null)
+})
+app.listen(3000)
+
+console.log('Server running on http://localhost:3000')
