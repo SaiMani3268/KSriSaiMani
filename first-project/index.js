54c61db3d5ece9df9254196e99934e2d9610d81c
Kovvada Sri Sai Mani <srisaimanikovvada@gmail.com>
2022-06-29 23:50:17 +0530

    fifth commit

--- first-project/index.js
+++ first-project/index.js
@@ -9,20 +9,24 @@ app.set('view engine', 'mustache')
 app.engine('mustache', require('hogan-middleware').__express)
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res, next) => {
-    res.send('Hello!!')
-})
+// app.get('/', (req, res, next) => {
+//     res.send('Hello!!')
+// })
 
-app.get('/json', (req, res, next) => {
-    const data = {
-        greeting: 'Hello!!'
-    }
-    res.json(data)
-})
+// app.get('/json', (req, res, next) => {
+//     const data = {
+//         greeting: 'Hello!!'
+//     }
+//     res.json(data)
+// })
 
-app.get('/home', (req, res, next) => {
-    res.render('home', null)
-})
+// app.get('/home', (req, res, next) => {
+//     res.render('home', null)
+// })
+
+const indexRouter = require('./routes/index')
+app.use('/', indexRouter)
+
 app.listen(3000)
 
 console.log('Server running on http://localhost:3000')
